8649a690a7f7078755894f6be1c64862af1d0aa4
zhangsf886 <15539293+zhangsf886@user.noreply.gitee.com>
2026-05-16 21:11:32 +0800

    取消代理

--- vite.config.js
+++ vite.config.js
@@ -19,15 +19,6 @@ export default defineConfig({
         // 场景 2：如果你的 NestJS 路由仅仅是 @Controller('auth')，你需要把 '/api' 抹掉，请取消下面这行的注释。
          rewrite: (path) => path.replace(/^\/api/, ''), 
       },
-      '/prod': {
-        target: 'http://18.216.195.130:8083', // 你的 NestJS 后端运行地址
-        changeOrigin: true,              // 开启跨域：将请求头中的 host 修改为 target
-        
-        // 【注意】：这取决于你的 NestJS 后端路由配置！
-        // 场景 1：如果你的 NestJS 配置了全局前缀 app.setGlobalPrefix('api')，则不需要下面这行。
-        // 场景 2：如果你的 NestJS 路由仅仅是 @Controller('auth')，你需要把 '/api' 抹掉，请取消下面这行的注释。
-         rewrite: (path) => path.replace(/^\/api/, ''), 
-      },
     },
   },
 })
